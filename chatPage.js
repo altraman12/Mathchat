2d259f52745b4f8ad622896ebc0a121d4829ef00
altraman12 <altraman12@gmail.com>
2015-03-25 17:34:16 -0700

    got rid of applet, fixed chat
    added chat history, made it work, fixed chat & duplicating issues and
    got rid of the applet since we're switching to servlets

--- chatPage.js
+++ chatPage.js
@@ -1,4 +1,7 @@
-var spreadSheetBridge;
+
+var text1 = "`x = (-b +- sqrt(b^2-4ac))/(2a) .`";
+var text2 = "`x = (-b +- sqrt(b^2-4ac))/(2a) .`";
+var text3 = "`x = (-b +- sqrt(b^2-4ac))/(2a) .`";
 
 function clearTextbox() {
     "use strict";
@@ -7,11 +10,25 @@ function clearTextbox() {
 
 function send() {
     "use strict";
-    document.getElementById("messages").innerHTML += "<br>" + document.getElementById("inputBox").value;
-    MathJax.Hub.Queue(["Typeset", MathJax.Hub]);
+    //document.getElementById("messages").innerHTML += "<br>" + document.getElementById("inputBox").value;
+    var space1 = document.getElementById("1");
+    var space2 = document.getElementById("2");
+    var space3 = document.getElementById("3");
+    
+    var math1 = MathJax.Hub.getAllJax("messages")[0];
+    var math2 = MathJax.Hub.getAllJax("messages")[1];
+    var math3 = MathJax.Hub.getAllJax("messages")[2];
+    
+    text1 = text2;
+    text2 = text3;
+    text3 = document.getElementById("inputBox").value;
+    console.log(text3);
+    
+    MathJax.Hub.Queue(["Text", math1, text1]);
+    MathJax.Hub.Queue(["Text", math2, text2]);
+    MathJax.Hub.Queue(["Text", math3, text3]);
+    //MathJax.Hub.Queue(["Typeset", MathJax.Hub]);
     clearTextbox();
-    spreadSheetBridge = document.getElementById('bridge');
-    spreadSheetBridge.paint();
 }
 
 var menuOpen = false;
